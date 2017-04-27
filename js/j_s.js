db08bc88bb58120fcc294ddbcd712b601ef925ac
HZH <HZH@058c1746-8e46-4805-bda7-7a1ea76d6762>
2017-04-27 01:32:37 +0000

    完成购物车动态物品显示

--- js/j_s.js
+++ js/j_s.js
@@ -31,7 +31,7 @@ function Selected(evenr) { //购物车选中物品事件
 				   if($("#" + shopping_id + "").find('input[type="checkbox"]').is(":checked")){
 				   	$("#Total").html($(this).val());
 				   }else{
-				   	mui.alert("都没选你改啥");
+//				   	mui.alert("都没选你改啥");
 				   }
 				   
 				});  
@@ -55,77 +55,9 @@ function Selected(evenr) { //购物车选中物品事件
 
 
 
-
-
-function getAjax(url, datas, successcallback, errorcallback) { //被调用的函数
-	mui.ajax(url, { //mui.ajax是AJAX的规定前缀写法，后面的url是服务器的路径（库，表等等都在服务器内）
-		data: JSON.stringify(datas), //也就是键值对可以
-		contentType: "application/json", //解析你的内容是什么格式
-		type: "post", //请求发送的方式，有post(快，安全低),get(慢，安全高)
-		dataType: "json", //服务器返回的类型是什么，有"xml"(返回XML文档),"html"(返回纯文本HTML信息),"script"(返回纯文本JavaScript代码),"json"(返回JSON数据),"text"(返回纯文本字符串),
-		timeout: 10000, //请求超时时间（毫秒），默认值为0，表示永不超时，如果超过请求时间还未依为收到，则失败
-		success: function(da) { //正确的回调函数
-			successcallback(da.d);
-		},
-		error: function(xhr, type, errorThrown) { //错误的回调函数
-			errorcallback(xhr, type, errorThrown)
-		}
-	});
-}
-
 function setIten(key, value) { //被调用的函数
 	plus.storage.setItem(key, value); //存储键值对
 }
-
-
-var men;
-
-function Addend(even) { //商品加
-	men = even.previousElementSibling.innerHTML;
-	men++;
-	even.previousElementSibling.innerHTML = men;
-	var parEven = even.parentNode.parentNode.parentNode.parentNode;
-	var parPrice = even.parentNode.parentNode;
-	var evenPrice = parPrice.childNodes[1].childNodes[1].innerHTML; //获取价格
-	var chiParEven = parEven.childNodes[2].childNodes[0].childNodes[1].childNodes[1]; //小计的标签所在
-	var chiChe = parEven.childNodes[0].childNodes[0].childNodes[1]; //获取复选框标签
-	var evrTotal = document.getElementById("Total").innerHTML;
-	var parTotal = parseFloat(evrTotal); // 获取转换为字符型的合计总价
-	var parPrice = parseFloat(evenPrice); // 获取转换为字符型的价格
-	chiParEven.innerHTML = men * evenPrice;
-	if(chiChe.checked == true) {
-		document.getElementById("Total").innerHTML = parTotal + parPrice;
-	} else {
-
-	}
-
-}
-
-function Meiosis(meng) { //商品减
-	men = meng.nextSibling.innerHTML;
-	if(men == 1) {
-		return false;
-	} else {
-		men--;
-		meng.nextSibling.innerHTML = men;
-		var parEven = meng.parentNode.parentNode.parentNode.parentNode;
-		var parPrice = meng.parentNode.parentNode;
-		var evenPrice = parPrice.childNodes[1].childNodes[1].innerHTML;
-		var chiParEven = parEven.childNodes[2].childNodes[0].childNodes[1].childNodes[1];
-		var chiChe = parEven.childNodes[0].childNodes[0].childNodes[1]; //获取复选框标签
-		var evrTotal = document.getElementById("Total").innerHTML;
-		var parTotal = parseFloat(evrTotal); // 获取转换为字符型的合计总价
-		var parPrice = parseFloat(evenPrice); // 获取转换为字符型的价格
-		chiParEven.innerHTML = men * evenPrice;
-		if(chiChe.checked == true) {
-			document.getElementById("Total").innerHTML = parTotal - parPrice;
-		} else {
-
-		}
-	}
-}
-var several = 0;
-
 
 
 function deleteOrder(event) {
